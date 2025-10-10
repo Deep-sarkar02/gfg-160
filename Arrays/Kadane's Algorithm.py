dd8be4cfbae18459c44ab5cad46331de960f63f6
Chndrangshu sarkar <121252899+Deep-sarkar02@users.noreply.github.com>
2025-10-10 23:13:59 +0530

    Update Kadane's Algorithm.py

--- Arrays/Kadane's Algorithm.py
+++ Arrays/Kadane's Algorithm.py
@@ -1,4 +1,4 @@
- given a integer array arr[]. You need to find the maximum sum of a subarray (containing at least one element) in the array arr[].
+ given a integer array arr[]. You need to find the maximum sum of a subarray (containing at least one element in the array arr[].
 
 Note : A subarray is a continuous part of an array.
 
